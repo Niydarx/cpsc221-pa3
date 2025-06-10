81a43d1ae6e2013494592d1e2d9ce911a07d0d89
Niydarx <ngerichg@gmail.com>
2025-06-10 22:57:42 +0000

    Declared an RGBSum struct and helper functions.

--- hextree-private.h
+++ hextree-private.h
@@ -9,5 +9,36 @@
  *              No other scaffolding is necessary.
  */
 
- // begin your declarations below
+// begin your declarations below
+struct RGBSum
+{
+    long r, g, b;
+};
 
+/**
+ * summedAreaTable is a 2D vector of RGBSums
+ * Contains the cumalative r, g, b values of any pixel from [x][y] to origin 
+ */
+vector<vector<RGBSum>> summedAreaTable;
+
+/**
+ * Returns an RGBSum with the sums the values of two given RGBSums
+ * @param a An RGBSum
+ * @param b An RGBSum
+ */
+RGBSum addRGBASums(RGBSum a, RGBSum b);
+
+/**
+ * Returns an RGBSum with the difference the values of two given RGBSums
+ * @pre a > b
+ * @param a An RGBSum
+ * @param b RGBSum to subtract from a
+ */
+RGBSum subtractRGBASums(RGBSum a, RGBSum b);
+
+// Returns an RGBSum given an RGBAPixel
+RGBSum pixelToSum(const RGBAPixel p);
+
+void createSummedAreaTable(unsigned int imageWidth, unsigned int imageHeight, const PNG & imIn);
+
+RGBAPixel calculateAverage(pair<unsigned int, unsigned int> ul, pair<unsigned int, unsigned int> lr);
